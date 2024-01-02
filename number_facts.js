169a433cd3289a52c087b060c1637554dd3a0d3a
Crystal Tran <c.t.tran95@gmail.com>
2024-01-01 21:16:37 -0500

    filtered for okResponses in showNumberAll

--- number_facts.js
+++ number_facts.js
@@ -6,8 +6,8 @@ const NUMBERS_API_URL = "http://numbersapi.com";
  * Numbers API to receive trivia about that number. Console logs the
  * trivia.
  */
-async function showNumberTrivia(num){
-  const response = await fetch(`${NUMBERS_API_URL}/${num}?json`)
+async function showNumberTrivia(num) {
+  const response = await fetch(`${NUMBERS_API_URL}/${num}?json`);
   // {
   //   method: 'GET',
   //   headers:{
@@ -16,14 +16,14 @@ async function showNumberTrivia(num){
   // });
   const numData = await response.json();
   console.log('response is:', response, 'numData is:', numData);
-  console.log("Num trivia for is:", numData.text );
+  console.log("Num trivia for is:", numData.text);
 }
 
 /** Receives four integer number inputs and makes a fetch request to the
  * Numbers API. Runs promise.race to console log the first trivia answer
  * that returns.
  */
-async function showNumberRace(num1, num2, num3, num4){
+async function showNumberRace(num1, num2, num3, num4) {
   const response1 = fetch(`${NUMBERS_API_URL}/${num1}?json`);
   const response2 = fetch(`${NUMBERS_API_URL}/${num2}?json`);
   const response3 = fetch(`${NUMBERS_API_URL}/${num3}?json`);
@@ -37,14 +37,39 @@ async function showNumberRace(num1, num2, num3, num4){
 }
 
 
-async function showNumberAll(numList){
+async function showNumberAll(numList) {
   const numPromises = [];
-  for (let num of numList){
+  for (let num of numList) {
     const response = fetch(`${NUMBERS_API_URL}/${num}?json`);
     numPromises.push(response);
   }
 
-  const results = await Promise.allSettled(numPromises)
-  console.log(results)
+  const results = await Promise.allSettled(numPromises);
+  console.log("results:", results);
+
+  const numData = [];
+  for(let result of results){
+    const data = await result.value.json();
+    numData.push(data);
+  }
+
+  // const numData = results.map( r => r.json())
+
+  console.log('numData is', numData);
+
+  const okResponses = results.filter(r => r.status === "fulfilled" && r.value.ok === true)
+  const trivias = okResponses.map(r => r.value.json());
+  console.log('trivias is:', trivias);
+
+
+
+  console.log('okResponses', okResponses);
+  // const test = await Promise.all(okResponses);
+  // return test;
+  // const promiseResolved = results.filter( x => x.value.status = 200);
+  // const promiseRejected = results.filter( x => x.value.status = 404);
+
+  // console.log('showNumber All fulfilled:', promiseResolved,
+  // 'showNumber All rejected:', )
 }
 //FIXME: random number generator?
